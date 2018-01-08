80f442c70ebe3fc90c3ea456e8d13b8528c5dcfe
nichpuet <napuetz@gmail.com>
2018-01-08 13:35:06 -0500

    u7h

--- TexasHoldEmPoker/Form1.Designer.cs
+++ TexasHoldEmPoker/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TexasHoldEmPoker
 {
-    partial class Form1
+    partial class Poker
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Poker));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.playButton = new System.Windows.Forms.Button();
+            this.optionButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(318, 58);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(112, 13);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Texas Hold \'Em Poker";
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(333, 148);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 23);
+            this.playButton.TabIndex = 1;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // optionButton
+            // 
+            this.optionButton.Location = new System.Drawing.Point(333, 209);
+            this.optionButton.Name = "optionButton";
+            this.optionButton.Size = new System.Drawing.Size(75, 23);
+            this.optionButton.TabIndex = 2;
+            this.optionButton.Text = "Options";
+            this.optionButton.UseVisualStyleBackColor = true;
+            this.optionButton.Click += new System.EventHandler(this.optionButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(333, 268);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Poker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(734, 661);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.optionButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.titleLabel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Poker";
+            this.Text = "Texas Hold \'Em Poker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button optionButton;
+        private System.Windows.Forms.Button exitButton;
     }
 }
 
--- TexasHoldEmPoker/Form1.cs
+++ TexasHoldEmPoker/Form1.cs
@@ -8,13 +8,46 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Nick Puetz | 1/8/1018 | Texas Hold em Poker
+
 namespace TexasHoldEmPoker
 {
-    public partial class Form1 : Form
+    public partial class Poker : Form
     {
-        public Form1()
+        public Poker()
         {
             InitializeComponent();
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            Graphics fG = this.CreateGraphics();
+            SolidBrush tableBrush = new SolidBrush(Color.Olive);
+            Pen briefPen = new Pen(Color.Black,5);
+
+            titleLabel.Visible = false;
+            playButton.Visible = false;
+            optionButton.Visible = false;
+            exitButton.Visible = false;
+
+            BackColor = Color.SandyBrown;
+
+            System.Threading.Thread.Sleep(1);
+            Refresh();
+
+            fG.DrawLine(briefPen, 0, 500, 750, 500);
+           // fG.FillEllipse(tableBrush, 150, , 350, 300);
+            
+        }
+
+        private void optionButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
